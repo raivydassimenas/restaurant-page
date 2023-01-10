18386c23238c175bbfec84659152ef22d1c54960
Raivydas Šimėnas <raivydas.simenas@gmail.com>
2023-01-10 18:22:00 +0200

    Add tabs

--- src/index.js
+++ src/index.js
@@ -1,10 +1,44 @@
 import loadRestaurantTab from './restaurant';
 import loadMenuTab from './menu';
 import loadContactTab from './contact';
+import "./styles.css";
 
 const contentDiv = document.querySelector("#content");
+
 const restaurantDiv = loadRestaurantTab();
 const menuDiv = loadMenuTab();
 const contactDiv = loadContactTab();
 
-contentDiv.append(contactDiv);
+const menuBar = document.createElement("div");
+menuBar.classList.add("menu-bar");
+
+const restaurantButton = document.createElement("button");
+restaurantButton.innerText = "Restaurant"
+restaurantButton.addEventListener("click", e => {
+    contentDiv.innerHTML = "";
+    contentDiv.appendChild(menuBar);
+    contentDiv.appendChild(restaurantDiv);
+});
+menuBar.appendChild(restaurantButton);
+
+const menuButton = document.createElement("button");
+menuButton.innerText = "Menu"
+menuButton.addEventListener("click", e => {
+    contentDiv.innerHTML = "";
+    contentDiv.appendChild(menuBar);
+    contentDiv.appendChild(menuDiv);
+});
+menuBar.appendChild(menuButton);
+
+const contactButton = document.createElement("button");
+contactButton.innerText = "Contact"
+contactButton.addEventListener("click", e => {
+    contentDiv.innerHTML = "";
+    contentDiv.appendChild(menuBar);
+    contentDiv.appendChild(contactDiv);
+})
+menuBar.appendChild(contactButton);
+
+
+contentDiv.appendChild(menuBar);
+contentDiv.appendChild(restaurantDiv);
